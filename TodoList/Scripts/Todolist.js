2f74298e0be9cc8d12b69b0cc2763fe03fae4ee0
Smita Shetty <smita.narasimha@gmail.com>
2016-12-24 23:29:26 +1100

    First upload

---
+++ TodoList/Scripts/Todolist.js
@@ -0,0 +1,102 @@
+﻿angular.module('todoApp', [])
+       .controller('MainController', ['ListService',function (ListService) {
+           var self = this;
+
+           self.getClass = function (status) {
+               return {
+                   Done: status,
+                   Pending: !status
+               };
+           };
+           //todo: add test file for this to git
+           self.isValidItem = function () {
+               if (self.item === null) { return false; }
+               if (self.item === undefined) { return false; }
+               var Name,Desc;
+
+               if (self.item.Name !== undefined)
+               {
+                   Name = self.item.Name;
+               }
+               if (self.item.Desc !== undefined)
+               {
+                   Desc = self.item.Desc;
+               }
+
+               console.log(Name+" "+Desc);
+
+               if ((Name === undefined) || (Name === null))
+               {
+                   Name = "";
+               }
+               
+               if ((Desc === undefined) || (Desc === null))
+                {
+                   Desc = "";
+               }
+
+               if((Name.trim()==="") && (Desc.trim()===""))
+               {
+                   return false;
+               }               
+
+               return true;
+           };
+
+           self.AddNew = function () {
+               if (self.isValidItem() == true) {
+                   var newItem = {
+                       Name: self.item.Name,
+                       Desc: self.item.Desc,
+                       Done: self.item.Done
+                   };
+                   console.log(self.getList());
+                   ListService.Add(newItem);
+               }
+           };
+
+           self.getSortedList = function () {
+               return ListService.GetAllItemsSortedByDone();
+           };
+
+           self.getList = function () {
+               return ListService.List();
+           };       
+
+           self.delItem = function (Item)
+           {
+               ListService.RemoveItem(Item);
+           }
+       }])
+
+    .factory('ListService', [function () {
+        var self = this;
+        self.TodoList = [];
+
+        return ({
+            RemoveItem: function (Item) {
+                var _removeIndex = self.TodoList.indexOf(Item);
+            
+                if (_removeIndex > -1)
+                {
+                   self.TodoList.splice(_removeIndex, 1);
+                }
+            },
+
+            List: function () {
+                return self.TodoList;
+            },
+
+            Add: function (Item) {
+                   self.TodoList.push(Item);                    
+            },
+
+             GetAllItemsSortedByDone: function () {
+                 return self.TodoList.sort(function (a) {
+                    var x = (a.Done ===true) ? 1 : -1;
+                    return x;
+                });
+            }
+        }
+        );
+}]);
---
+++ TodoList/Scripts/test/todoAppTests.js
@@ -0,0 +1,71 @@
+﻿describe('MainController', function () {
+    beforeEach(module('todoApp'));
+
+    var ctrl,listservice;
+
+    beforeEach(inject(function ($controller, ListService) {
+        listservice = ListService;
+        ctrl = $controller('MainController');      
+    }));
+
+it('should have no items on load', function () {
+    expect(ctrl.getList()).toEqual([]);
+});
+
+it('should retrn a sorted list sorted on Done for true value t, with done===true in the end', function () {
+    var targetTodoList = [
+        { Name: "A", Desc: "A Description", Done: false },
+        { Name: "B", Desc: "B Description", Done: false},
+        { Name: "C", Desc: "C Description", Done: true }
+    ];
+    var actualItemToLookFor = { Name: "A", Desc: "A Description", Done: true };
+    listservice.Add(targetTodoList[0]);
+    listservice.Add(targetTodoList[1]);
+    listservice.Add(targetTodoList[2]);
+
+    var actualSortedList = ctrl.getSortedList();
+    expect(actualSortedList).toBeDefined();
+ 
+    var actualIndexOfIntendedItem = actualSortedList.findIndex(x => x.Name=="C");
+    console.log(actualIndexOfIntendedItem);
+
+    var targetIndexOfIntendedItem = targetTodoList.length-1;
+    
+    expect(targetIndexOfIntendedItem).toEqual(actualIndexOfIntendedItem);
+});
+
+it('should return a non empty list', function () {
+    var targetTodoList = [{ Name: "A", Desc: "A Description", Done: true }];
+
+    ctrl.item = { Name: "New item name", Desc: "New item Description", Done: false };
+
+    //if thiss works.... 
+    ctrl.AddNew();
+
+    //check if I can set order of execution on these tests so Add test can run before non empty test
+    var actualTodoList = ctrl.getList();
+
+    expect(actualTodoList).toBeDefined();
+    expect(actualTodoList.length).toEqual(1);
+});
+
+it('should return a list without deleted item', function () {
+    var toDeleteItem = { Name: "delete", Desc: "Delete me", Done: true };
+    
+    listservice.Add({Name:"A",Desc:"A Desc",Done: true});
+    listservice.Add(toDeleteItem);
+
+    var listLen=2;
+    expect(ctrl.getList().length).toEqual(listLen);
+
+    ctrl.delItem(toDeleteItem);
+
+    expect(ctrl.getList().length).toEqual(listLen-1);
+   
+});
+    //Testing service api
+it('should return a list with added item', function () {
+    listservice.Add({Name:"some name", Desc:"some desc", Done:true});
+    expect(listservice.List().length===1).toBeTruthy();    
+});
+});
